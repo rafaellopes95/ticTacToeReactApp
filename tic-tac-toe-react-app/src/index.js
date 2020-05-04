73012d7c87ef4067993b7d0ec187ef333d3b48af
Rafael Lopes <rafa_lopes_leme@hotmail.com>
2020-05-03 22:37:01 -0300

    Functional version of the game.

--- tic-tac-toe-react-app/src/index.js
+++ tic-tac-toe-react-app/src/index.js
@@ -2,25 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  // this.props.value will show the value contained in the props passed by parent component (Board).
-  render() {
-    return (
-      <button className="square" onClick={() => alert('click')}>
-        {this.props.value}
-      </button>
-    );
-  }
+// Transformed Square class into a function component, which is simpler, can contain a single render method only and cannot keep state.
+function Square(props) {
+  return (
+    <button 
+      className="square" 
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  
+  // Created constructor so it was possible to use the state (which is private).
+  // The state contains an Array with a position dedicated for each square.
+  // The super constructor call is mandatory in all React components.
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    // Using slice to create a copy from the original array, avoiding mutability.
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  
   renderSquare(i) {
-    // Passing props to child component (Square).
-    return <Square value={i}/>;
+    // Passing the state to the Square component.
+    // The handleClick function will be sent to the Square component so it uses it during onClick event.
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    ); 
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -59,6 +94,27 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+// Custom function to check if there is a winner in the game, checking all the possibilities.
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
