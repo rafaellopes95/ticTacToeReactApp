db5d581004c44e9c6d749bba75e0a2e6baf051ca
Rafael Lopes <rafa_lopes_leme@hotmail.com>
2020-05-10 15:51:44 -0300

    Moved the state to the higher component class, which is the Game class. Also, the history of the game is kept there, and no more plays are allowed once there is a winner. The handleClick function was also sent to the Game class, and this is shared with Board class via props. Then, the Board class passes the handleClick function over to Square function via props too.

--- tic-tac-toe-react-app/src/index.js
+++ tic-tac-toe-react-app/src/index.js
@@ -16,50 +16,20 @@ function Square(props) {
 
 class Board extends React.Component {
   
-  // Created constructor so it was possible to use the state (which is private).
-  // The state contains an Array with a position dedicated for each square.
-  // The super constructor call is mandatory in all React components.
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    // Using slice to create a copy from the original array, avoiding mutability.
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-  
   renderSquare(i) {
-    // Passing the state to the Square component.
-    // The handleClick function will be sent to the Square component so it uses it during onClick event.
+    // Passing the state from Game class to the Square component via props.
+    // The onClick function from Game will be sent to the Square component via props so it uses it during onClick event.
     return (
       <Square 
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     ); 
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if(winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -81,14 +51,65 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+
+  // Created constructor so it was possible to use the state (which is private).
+  // Ideally the state need to be handled by the highest level component as possible, which is Game class in this case.
+  // The state contains an Array with a position dedicated for each square.
+  // The super constructor call is mandatory in all React components.
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    // Using slice to create a copy from the original array, avoiding mutability.
+    // Immutability here will allow to keep the match history.
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if(calculateWinner(squares || squares[i])) {
+      return;
+    }
+    
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+    })
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    
+    let status;
+
+    if(winner) {
+      status = 'Winner is: ' + winner;
+    } else {
+      status = 'Next player is: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
